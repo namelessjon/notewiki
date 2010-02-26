98c4376ba9412fb5119affb22b7ef27122ef617e
Jonathan Stott <jonathan.stott@gmail.com>
2010-02-26 12:20:42 +0000

    Moved html to layout

--- shows/post.js
+++ shows/post.js
@@ -1,18 +1,21 @@
 function(doc, req) {
   // !json templates.post
+  // !json templates.layout
   // !json couchapp
   // !code helpers/template.js
   // !code vendor/couchapp/path.js
 
+
   // we only show http
-  return template(templates.post, {
-name: couchapp.name,
+  return template(templates.layout, {
+    name: couchapp.name,
 //    title : doc.title,
 //    blogName : blog.title,
 //    post : doc.html,
 //    date : doc.created_at,
 //    author : doc.author,
-    assets : assetPath()
+    assets : assetPath(),
+    content: template(templates.post, {})
 //    editPostPath : showPath('edit', doc._id),
 //    index : listPath('index','recent-posts',{descending:true, limit:5})
   });
