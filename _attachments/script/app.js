0b6dd1226e81a193a18d4562fe3dbf6f43028a11
Jonathan Stott <jonathan.stott@gmail.com>
2010-03-05 00:22:58 +0000

    Using evently now
    Should probably make templates mustaches, too

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -1,78 +1,13 @@
-;(function($)
-{
-    // User class taken from swinger, by quirkey
-    // Copyright (c) 2009 Aaron Quint
-    // http://github.com/quirkey/swinger
-  User = {
-    _current_user: false,
-    isLoggedIn: function() {
-      return !!this._current_user;
-    },
-    current: function(callback, force) {
-      var user = this;
-      if (!this._current_user || force === true) {
-        $.couch.session({
-          success: function(session) {
-            if (session.userCtx && session.userCtx.name) {
-              user._current_user = session.userCtx;
-              callback(user._current_user);
-            } else {
-              user._current_user = false;
-              callback(false);
-            }
-          }
-        });
-      } else {
-        callback(user._current_user);
-      }
-    },
-    login: function(name, password, callback) {
-      $.couch.login({
-        name : name,
-        password : password,
-        success: function() {
-          User.current(callback, true);
-        },
-        error: function(code, error, reason) {
-          showNotification('error', reason);
-        }
-      });
-    },
-    logout: function(callback) {
-      var user = this;
-      $.couch.logout({
-        success: function() {
-          user._current_user = false;
-          callback();
-        },
-        error: function(code, error, reason) {
-          showNotification('error', reason);
-        }
-      });
-    },
-    signup: function(name, email, password, callback) {
-      $.couch.signup({name: name, email: email}, password, {
-        success: function() {
-          User.login(name, password, callback);
-        },
-        error: function(code, error, reason) {
-          showNotification('error', reason);
-        }
-      })
-    }
-  };
+$.couch.app(function(app) {
+  // setup the account and profile widgets
+  // extend the vendor profile widget with Toast specific code
+  var acct = $.extend(true, {},
+    app.ddoc.vendor.couchapp.evently.account,
+    app.ddoc.evently.account);
+  $("#account").evently(acct, app);
 
+//  $("#profile").evently(app.ddoc.vendor.couchapp.evently.profile, app);
 
-})(jQuery);
+//  $.evently.connect($("#account"), $("#profile"), ["loggedIn", "loggedOut"]);
 
-$(function() {
-    User.current(function(user) {
-        if (user) {
-            $('#basic_account').html("Hello " + user.name);
-        } else { 
-            $('#basic_account').html("Hello anon");
-        }
-
-
-    });
 });
